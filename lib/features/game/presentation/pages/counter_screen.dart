a94afed3ca7b8304d01513d7f98ccdfeb8e9c317
Mücahit <mctozal@gmail.com>
2025-02-16 21:06:49 +0300

    counter, pov screens are created.

---
+++ lib/features/game/presentation/pages/counter_screen.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  final int roundNumber;
+  final VoidCallback onCountdownComplete;
+
+  const CounterScreen(
+      {super.key,
+      required this.roundNumber,
+      required this.onCountdownComplete});
+
+  @override
+  _CounterScreenState createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int _counter = 5; // Start from 5
+
+  @override
+  void initState() {
+    super.initState();
+    _startCountdown();
+  }
+
+  void _startCountdown() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_counter == 1) {
+        timer.cancel();
+        widget.onCountdownComplete(); // Move to the next screen
+      } else {
+        setState(() {
+          _counter--;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: Image.asset(
+                'lib/assets/logo-big.png', // Game logo
+                height: 100,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Round ${widget.roundNumber}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Starts in $_counter',
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/game/presentation/pages/pov_screen.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class PoseScreen extends StatefulWidget {
+  final VoidCallback onPoseComplete;
+  final String pose;
+
+  const PoseScreen(
+      {super.key, required this.onPoseComplete, required this.pose});
+
+  @override
+  _PoseScreenState createState() => _PoseScreenState();
+}
+
+class _PoseScreenState extends State<PoseScreen> {
+  double _progress = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _startProgress();
+  }
+
+  void _startProgress() {
+    Timer.periodic(Duration(milliseconds: 300), (timer) {
+      if (_progress >= 1.0) {
+        timer.cancel();
+        widget.onPoseComplete(); // Move to game screen
+      } else {
+        setState(() {
+          _progress += 0.1;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          LinearProgressIndicator(
+            value: _progress,
+            backgroundColor: Colors.grey,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+          ),
+          Expanded(
+            child: Center(
+              child: Text(
+                widget.pose,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
